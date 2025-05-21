1f185955dc3578fa20a4398fd164a0b34d61f21d
Patryk Wieczorek <patryk@wieczorek.dev>
2025-05-21 08:10:48 +0200

    leetcode day

---
+++ leetcode/238.py
@@ -0,0 +1,38 @@
+from typing import List
+
+def productExceptSelf(nums: List[int]) -> List[int]:
+    # 1. create prefix and suffix product arrays
+    prefix = [nums[0]]
+    suffix = [nums[len(nums) - 1]]
+    for i in range(1, len(nums), 1):
+
+        prefix.append(prefix[i - 1] * nums[i])
+        suffix.append(suffix[i - 1] * nums[len(nums) - i - 1])
+    suffix.reverse()
+    
+    print(f"{prefix=}")
+    print(f"{suffix=}")
+    print(f"{nums=}")
+    # solution is prefix[i-1] * suffix[i+1]
+    solution = [suffix[1]]
+    for i in range(1, len(nums) - 1, 1):
+        solution.append(prefix[i - 1] * suffix[i + 1])
+    # last is just last prefix nvm its good nvm its wrong xD
+    solution.append(prefix[-2])
+    print(f"{solution=}")
+    print("-----")
+    return solution
+
+
+nums = [1,2,3,4]
+print(productExceptSelf(nums))
+
+
+nums = [-1,1,0,-3,3]
+print(productExceptSelf(nums))
+
+nums = [2,1]
+print(productExceptSelf(nums))
+
+nums = [-3, 1, 3]
+print(productExceptSelf(nums))
--- leetcode/3sum.py
+++ leetcode/3sum.py
@@ -1,24 +1,48 @@
 from typing import List
 
 def threeSum(nums: List[int]) -> List[List[int]]:
-        # indices dont really matter, its about numbers
-        nums = sorted(nums)
-        left = 0
-        right = len(nums) - 1
-        missing_pieces = {}
-        # i can filter incorrect triplets 
-        while left < right:
-        # as left is the smallest number, we wont find a smaller number 
-        # as right is the biggest number, we wont find a bigger number
-            missing_piece = nums[right] + nums[left]
-            if nums[left] > missing_piece > nums[right]:
-                missing_pieces[missing_piece].append([nums[right], nums[left]])
-                left += 1
-            elif missing_piece > nums[right]:
-                right -= 1
-            elif missing_piece < nums[left]:
-                left += 1
-        print(missing_pieces)
+    # indices dont really matter, its about numbers
+    triplets = []
+    nums = sorted(nums)
+    fixd = 0
+    prev = None
+    while fixd < len(nums) - 2:  
+        # fix the number, use it as target for 2sum
+        # a + b + c = 0 -> a + b = -c
 
-nums = [-1,0,1,2,-1,-4]
+        target = -1 * nums[fixd]
+        if nums[fixd] != prev:
+            triplets.extend(twoSum(nums, fixd + 1, target))
+        prev = nums[fixd]
+        fixd += 1
+        
+    return triplets
+
+                
+def twoSum(nums, start, target):
+    # here we want to find 2 numbers that sum up to a fixed number 
+    l = start
+    r = len(nums) - 1
+    two_sums = []
+    prev_l = None
+    prev_r = None
+    # we need duplicate skipping mechanism in place
+    while l < r: # array is sorted, we can two pointer it
+        two_sum = nums[l] + nums[r]
+        if two_sum == target:
+            if nums[l] != prev_l and nums[r] != prev_r:
+                two_sums.append([-target, nums[l], nums[r]])
+            prev_l = nums[l]
+            l += 1
+        elif two_sum > target:
+            prev_r = nums[r]
+            r -= 1
+        elif two_sum < target:
+            prev_l = nums[l]
+            l += 1
+
+    print(f"{target=}, {two_sums=}")
+    return two_sums
+
+nums = [0,0,0,0]
 print(threeSum(nums))
