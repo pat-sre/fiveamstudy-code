feef1e559c681e530e0d5f13788a875156d8e670
Patryk Wieczorek <patryk@wieczorek.dev>
2025-05-24 08:51:47 +0200

    leetcode

--- leetcode/73.py
+++ leetcode/73.py
@@ -32,19 +32,31 @@ def setZeroes1(matrix: List[List[int]]) -> None:
 def setZeroes(matrix):
         m = len(matrix)
         n = len(matrix[0])
+        first_col_has_zero = False
+        first_row_has_zero = False
         for row in range(m):
             for col in range(n):
                 if matrix[row][col] == 0:
                     print(row, col)
                     matrix[0][col] = 0
                     matrix[row][0] = 0
-        print(matrix)
-        for row in range(m):
-            for col in range(n):
+                    if row == 0:
+                        first_row_has_zero = True
+                    if col == 0:
+                        first_col_has_zero = True
+
+        for row in range(1, m):
+            for col in range(1, n):
                 if matrix[0][col] == 0 or matrix[row][0] == 0:
-                    print(f"[{row}, {col}],  {matrix[0][col]=} or {matrix[row][0]=}")
                     matrix[row][col] = 0
+        if first_col_has_zero: # we have to zero 1st col
+            for i in range(len(matrix)):
+                matrix[i][0] = 0
+        if first_row_has_zero:
+            for i in range(len(matrix[0])):
+                matrix[0][i] = 0
         print(matrix)
+
 
 matrix = [[1,1,1],[1,0,1],[1,1,1]]
 setZeroes(matrix)
